3264876850e3bd577a7ce4934ed8826986700949
Lucas <pslucasps@gmail.com>
2017-05-16 10:51:55 -0300

    Inicializando projeto.

---
+++ src/br/inf/ocorrencias/testes/TesteExemplo.java
@@ -0,0 +1,14 @@
+package br.inf.ocorrencias.testes;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TesteExemplo {
+
+	@Test
+	public void teste() throws Exception {
+		assertTrue(true);
+	}
+
+}
