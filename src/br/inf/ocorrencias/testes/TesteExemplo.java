52d2b95661f7ed291cd401ef2808b85acd7e6575
Lucas <pslucasps@gmail.com>
2017-05-16 11:03:58 -0300

    Primeiro commit do ramo alternativo.

--- src/br/inf/ocorrencias/testes/TesteExemplo.java
+++ src/br/inf/ocorrencias/testes/TesteExemplo.java
@@ -1,5 +1,6 @@
 package br.inf.ocorrencias.testes;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,6 +10,7 @@ public class TesteExemplo {
 	@Test
 	public void teste() throws Exception {
 		assertTrue(true);
+		assertFalse(false);
 	}
 
 }
